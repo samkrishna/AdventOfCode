b622a67bf3b76df495d410cdd49bd71776b5704a
Sam Krishna <sam.krishna@gmail.com>
2018-12-27 13:23:48 -0800

    [Apple] Add framework header

---
+++ 2018/applelang/applelang/applelang.h
@@ -0,0 +1,20 @@
+//
+//  applelang.h
+//  applelang
+//
+//  Created by Sam Krishna on 12/26/18.
+//  Copyright © 2018 SectorMobile. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "RegexKitX.h"
+
+//! Project version number for applelang.
+FOUNDATION_EXPORT double applelangVersionNumber;
+
+//! Project version string for applelang.
+FOUNDATION_EXPORT const unsigned char applelangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <applelang/PublicHeader.h>
+
+
