0dee83aa02439225422c8f7d4aff6b3cd3e3ef3c
Sam Krishna <sam.krishna@gmail.com>
2019-01-03 05:51:00 -0800

    [Swift] Rename framework header to AOCSwift.h

---
+++ 2018/applelang/SwiftSupplement/AOCSwift.h
@@ -0,0 +1,19 @@
+//
+//  AOCSwift.h
+//  AOCSwift
+//
+//  Created by Sam Krishna on 12/26/18.
+//  Copyright © 2018 SectorMobile. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AOCSwift.
+FOUNDATION_EXPORT double AOCSwiftVersionNumber;
+
+//! Project version string for AOCSwift.
+FOUNDATION_EXPORT const unsigned char AOCSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOCSwift/PublicHeader.h>
+
+
